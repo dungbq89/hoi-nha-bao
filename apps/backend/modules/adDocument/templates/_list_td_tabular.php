03efda7de1dbc5b99a10b4ed76547254f89ff8f3
dungbq89 <dungbq89@gmail.com>
2015-06-01 23:15:54 +0700

    quản lý chuyên mục và quản lý tài liệu

---
+++ apps/backend/modules/adDocument/templates/_list_td_tabular.php
@@ -0,0 +1,7 @@
+  
+  <td class="sf_admin_text sf_admin_list_td_name" field="name">
+      <?php echo link_to(VtHelper::truncate($ad_document->getName(), 50, '...', true),'ad_document_edit',$ad_document)  ?>
+  </td>
+  <td class="sf_admin_foreignkey sf_admin_list_td_category_id" field="category_id"><?php echo  VtHelper::truncate($ad_document->getCategoryId(), 50, '...', true)  ?></td>      
+  <td class="sf_admin_text sf_admin_list_td_document_number" field="document_number"><?php echo  VtHelper::truncate($ad_document->getDocumentNumber(), 50, '...', true)  ?></td>      
+  <td class="sf_admin_text sf_admin_list_td_priority" field="priority"><?php echo  VtHelper::truncate($ad_document->getPriority(), 50, '...', true)  ?></td>    
