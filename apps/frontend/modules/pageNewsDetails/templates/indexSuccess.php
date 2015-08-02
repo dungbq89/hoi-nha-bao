4fb69b1d1c691ca48673da6bda98b72d55387cf9
ngoctv <ngoctv84@gmail.com>
2015-08-02 19:18:59 +0700

    chi tiet tin tuc

--- apps/frontend/modules/pageNewsDetails/templates/indexSuccess.php
+++ apps/frontend/modules/pageNewsDetails/templates/indexSuccess.php
@@ -27,7 +27,85 @@
             </div>
             <?php endif; ?>
             <div class="clear"></div>
+            <div class="commentTitle">
+                <h3>BÌNH LUẬN</h3>
+            </div>
         </div>
+
+        <div class="comment-form">
+
+            <form class="frm-log" action="" method="POST">
+                <?php echo $form->renderHiddenFields() ?>
+
+                <div class="frm-item">
+                    <span class="label">Họ tên (*)</span>
+                        <span class="btn-in">
+                             <?php echo $form['fullname']->render(array('class'=>'in-txt'));
+                             if ($form['fullname']->hasError()) {
+                                 echo '<span class="help-inline">' . $form['fullname']->renderError() . '</span>';
+                             }?>
+                        </span>
+                </div>
+
+
+                <div class="frm-item">
+                    <span class="label">Email (*)</span>
+                        <span class="btn-in">
+                             <?php echo $form['email']->render(array('class'=>'in-txt'));
+                             if ($form['email']->hasError()) {
+                                 echo '<span class="help-inline">' . $form['email']->renderError() . '</span>';
+                             }?>
+                        </span>
+                </div>
+
+                <div class="frm-item">
+                    <span class="label">Nội dung (*)</span>
+                        <span class="btn-in">
+                             <?php echo $form['content']->render(array('class'=>'in-txt'));
+                             if ($form['content']->hasError()) {
+                                 echo '<span class="help-inline">' . $form['content']->renderError() . '</span>';
+                             }?>
+                        </span>
+                </div>
+                <div class="frm-item">
+                    <span class="label"></span>
+                        <span class="btn-in">
+                            <?php if ($sf_user->hasFlash('success')): ?>
+                                <span><?php echo __($sf_user->getFlash('success'), null, 'tmcTwitterBootstrapPlugin') ?></span>
+                            <?php endif; ?>
+                        </span>
+                </div>
+                <div class="box-btn">
+                    <button name="" type="submit" class="btn">Gửi bình luận</button>
+                </div>
+
+            </form>
+        </div>
+        <?php
+         if(isset($articleOther) && count($articleOther)):
+        ?>
+            <div class="news-detail">
+                <div class="commentTitle">
+                    <h3>CÁC TIN ĐÃ ĐƯA</h3>
+                </div>
+                <?php
+                    foreach($articleOther as $value):
+                        $path = '/uploads/' . sfConfig::get('app_article_images') . $value['image_path'];
+                        $path = VtHelper::getThumbUrl($path, 460, 268, '');
+                ?>
+                    <div class="atc">
+                        <div style="background-image: url('<?php echo $path; ?>'); background-size: cover;" class="cover">
+                            <a href="<?php echo url_for2('article_detail',array('slug'=>$value['slug'])) ?>" title="<?php echo htmlspecialchars($value['title']); ?>"></a>
+                        </div>
+                        <div class="hed">
+                            <h1><a href="<?php echo url_for2('article_detail',array('slug'=>$value['slug'])) ?>">
+                                <?php echo VtHelper::truncate($value['title'],50,'...'); ?>
+                             </a></h1>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+         <?php endif; ?>
     </div>
 </div>
 <div class="col-right">
