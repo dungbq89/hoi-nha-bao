fc15d7974ef87198b37308181ac79cfd544a545e
dungbq89 <dungbq89@gmail.com>
2015-08-13 23:30:55 +0700

    thumb

--- apps/frontend/modules/moduleArticle/templates/_newArticle.php
+++ apps/frontend/modules/moduleArticle/templates/_newArticle.php
@@ -2,24 +2,37 @@
 if (isset($articles) && $articles):
     $path = '/uploads/' . sfConfig::get('app_article_images') . $articles[0]['image_path'];
     ?>
-    <div class="item item-hot-news">
-        <a href="<?php echo url_for2('article_detail',array('slug'=>$articles[0]['slug'])) ?>" title="<?php echo htmlspecialchars($articles[0]['title']); ?>">
-            <img class="img-news" src="<?php echo VtHelper::getThumbUrl($path, 460, 268, '') ?>" alt="">
-            <h4 class="title"><?php echo htmlspecialchars($articles[0]['alttitle']); ?></h4>
-        </a>
+    <div class="sct featured">
+        <div class="atc featured picture">
+            <div style="background-image: url(<?php echo VtHelper::getThumbUrl($path, 460, 268, '') ?>); background-size: cover;height: 268px;" class="cover dthumb" >
+                <a href="<?php echo url_for2('article_detail',array('slug'=>$articles[0]['slug'])) ?>"></a>
+            </div>
+            <div class="hed">
+                <h1><a href="<?php echo url_for2('article_detail',array('slug'=>$articles[0]['slug'])) ?>"><?php echo htmlspecialchars($articles[0]['alttitle']); ?></a></h1>
+                <time style="display:none"><?php
+                    if ($listNews[0]['published_time']) echo VtHelper::getFormatDate($articles[0]['published_time']);
+                    ?></time>
+                <p class="summary"><?php echo VtHelper::truncate($articles[0]['header'],200,'...'); ?></p>
+            </div>
+        </div>
+        <?php if (count($articles) > 1): ?>
+                <?php for($i=1;$i<4;$i++):
+                $pathi = '/uploads/' . sfConfig::get('app_article_images') . $articles[$i]['image_path'];
+                ?>
+                <div class="atc dor" style="<?php if($i==3) echo 'margin-right: 0px'; ?>">
+                    <a href="<?php echo url_for2('article_detail',array('slug'=>$articles[$i]['slug'])) ?>">
+                        <img style="height:100px;" src="<?php echo VtHelper::getThumbUrl($pathi, 150, 100, '') ?>">
+                    </a>
+                    <div class="hed"><h1><a href="<?php echo url_for2('article_detail',array('slug'=>$articles[$i]['slug'])) ?>"><?php echo htmlspecialchars($articles[$i]['alttitle']); ?></a></h1></div>
+                </div>
 
-        <p><?php echo VtHelper::truncate($articles[0]['header'],200,'...'); ?></p>
-<!--        <div class="thumb-hot">-->
-<!--            <div class="news-thumb">-->
-<!--                <a href="#"><img src="--><?php //echo VtHelper::getThumbUrl($path, 150, 100, '') ?><!--" /></a>-->
-<!--                <p>--><?php //echo VtHelper::truncate($articles[0]['header'],200,'...'); ?><!--</p>-->
-<!--            </div>-->
-<!--        </div>-->
+                <?php endfor; ?>
+        <?php endif; ?>
 
     </div>
     <div class="item item-lastest-news">
         <h3 class="title"><?php echo __('Tin nổi bật'); ?></h3>
-        <?php if (count($articles) > 1): ?>
+        <?php if (count($articles) > 4): ?>
             <ul class="box-scroll mCustomScrollbar">
                 <?php for($i=1;$i<count($articles);$i++): ?>
                 <li>
