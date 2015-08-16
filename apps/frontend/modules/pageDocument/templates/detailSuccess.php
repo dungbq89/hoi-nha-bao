839efb8a4d255bc7aac20b6297c75ede6f18ab45
ngoctv <ngoctv84@gmail.com>
2015-08-16 22:02:07 +0700

    link download trong chi tiet tai lieu

--- apps/frontend/modules/pageDocument/templates/detailSuccess.php
+++ apps/frontend/modules/pageDocument/templates/detailSuccess.php
@@ -11,30 +11,35 @@
     <div class="col-main">
     <div class="box">
     <h3 class="title-main"><span class="label"><?php echo __('Chi tiết văn bản'); ?></span></h3>
-    <table class="table bordered">
-<?php
-if ($document->file_path) {
-    $newLink = '/uploads/' . sfConfig::get('app_document') . '/' . $document->file_path;
-}
-?>
-    <tr>
-        <td class="td-document-detail">Tên văn bản</td>
-        <td><a style="color: #0000cc; font-weight: bold;" href="<?php echo $newLink; ?>" target="_blank"><?php echo htmlspecialchars($document->name); ?></a></td>
-    </tr>
-    <tr>
-        <td class="td-document-detail">Số văn bản</td>
-        <td><?php echo htmlspecialchars($document->document_number); ?></td>
-    </tr>
-    <tr>
-        <td class="td-document-detail">Ngày ban hành</td>
-        <td><?php echo date('d/m/Y',strtotime($document->getDocumentDate())); ?></td>
-    </tr>
-    <tr>
-        <td class="td-document-detail">Trích yếu</td>
-        <td><?php echo htmlspecialchars($document->description); ?></td>
-    </tr>
-    </table>
-
+        <table class="table bordered">
+            <?php
+            if ($document->file_path) {
+                $newLink = '/uploads/' . sfConfig::get('app_document') . '/' . $document->file_path;
+            }
+            ?>
+            <tr>
+                <td class="td-document-detail">Tên văn bản</td>
+                <td><a style="color: #3398cc; font-weight: bold;" href="<?php echo $newLink; ?>" target="_blank"><?php echo htmlspecialchars($document->name); ?></a></td>
+            </tr>
+            <tr>
+                <td class="td-document-detail">Số văn bản</td>
+                <td><?php echo htmlspecialchars($document->document_number); ?></td>
+            </tr>
+            <tr>
+                <td class="td-document-detail">Ngày ban hành</td>
+                <td><?php echo date('d/m/Y',strtotime($document->getDocumentDate())); ?></td>
+            </tr>
+            <tr>
+                <td class="td-document-detail">Trích yếu</td>
+                <td><?php echo htmlspecialchars($document->description); ?></td>
+            </tr>
+        </table>
+        <div class="download">
+            <a href="<?php echo $newLink; ?>" >
+                <img src="/img/download.png" style="border: none;">
+                <span>Tải xuống</span>
+            </a>
+        </div>
     </div>
     </div>
     <div class="col-right">
